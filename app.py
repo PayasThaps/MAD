5bfd3bc62f3571ea4f6ad702dd029870ee9add51
PayasThaps <payasthapliyal@gmail.com>
2025-01-16 16:45:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -168,5 +168,5 @@ def update_dashboard(selected_location, selected_event, selected_sentiment):
 
 # Run the app
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 11000))
     app.run_server(host="0.0.0.0", port=port)
